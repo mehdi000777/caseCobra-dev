c744bfd218cf69e960d5abbbcfd782426933445a
mehdi000777 <mehdi000077777@gmail.com>
2024-09-21 18:18:00 +0330

    second commit

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -17,7 +17,7 @@ const Page = () => {
 
   const { startUpload, isUploading } = useUploadThing('imageUploader', {
     onClientUploadComplete: ([data]) => {
-      const configId = data.serverData.configId
+      const configId = data?.serverData?.configId
       startTransition(() => {
         router.push(`/configure/design?id=${configId}`)
       })
